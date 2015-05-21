9b612c3d4fbaa98f3d8c910714b7ec1317d3751b
ikushin <you@example.com>
2015-05-21 17:52:53 +0900

    Update

--- go.sh
+++ go.sh
@@ -1,7 +1,13 @@
 
+sudo aptitude install -y ansible
+
 ansible-playbook -i ansible_hosts --list-hosts site.yml
 ansible-playbook -i ansible_hosts --list-tasks site.yml
 ansible-playbook -i ansible_hosts -vvvv site.yml
+
+sudo ceph status
+sudo ceph osd tree
+pgrep -lf ceph-
 
 #git diff --ignore-space-change --ignore-all-space --ignore-blank-lines --ignore-space-at-eol 58e656530009a91a0c48ed0e30c41737a7edf0e8
 
