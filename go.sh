b9aeea04d02a1cb282647b412c921e7733f70af7
ikushin <you@example.com>
2015-05-21 07:21:39 +0000

    Update

--- go.sh
+++ go.sh
@@ -2,3 +2,6 @@
 ansible-playbook -i ansible_hosts --list-hosts site.yml
 ansible-playbook -i ansible_hosts --list-tasks site.yml
 ansible-playbook -i ansible_hosts -vvvv site.yml
+
+#git diff --ignore-space-change --ignore-all-space --ignore-blank-lines --ignore-space-at-eol 58e656530009a91a0c48ed0e30c41737a7edf0e8
+
