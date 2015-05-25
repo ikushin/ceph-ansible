74409c525b716ab2f335ec3d702c4a641fe1ee5b
ikushin <you@example.com>
2015-05-25 18:23:51 +0900

    Update

--- go.sh
+++ go.sh
@@ -9,6 +9,4 @@ ansible-playbook -i ansible_hosts -vvvv site.yml
 sudo ceph status
 sudo ceph osd tree
 pgrep -lf ceph-
-
-#git diff --ignore-space-change --ignore-all-space --ignore-blank-lines --ignore-space-at-eol 58e656530009a91a0c48ed0e30c41737a7edf0e8
-
+sudo ceph-disk list
